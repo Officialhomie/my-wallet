4c59309d2b7643e7335dce66dee2dd3c73c9f8b3
OneTrueHomie <vicigwilo@gmail.com>
2026-01-06 21:15:49 +0100

    Remove wallet-farm/src/safety/ directory - moved to core/src/safety/

--- wallet-farm/src/safety/index.js
+++
@@ -1,4 +0,0 @@
-// Safety module exports
-export { BudgetEnforcer } from './BudgetEnforcer.js';
-export { CircuitBreaker, CircuitBreakerError } from './CircuitBreaker.js';
-export { RateLimiter } from './RateLimiter.js';
