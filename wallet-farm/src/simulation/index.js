ff9305da78f377183be5cd5b481da22923835848
OneTrueHomie <vicigwilo@gmail.com>
2026-01-06 21:17:50 +0100

    Remove wallet-farm/src/simulation/ directory - moved to core/src/simulation/

--- wallet-farm/src/simulation/index.js
+++
@@ -1,5 +0,0 @@
-// Simulation module exports
-export { ArchetypeManager } from './ArchetypeManager.js';
-export { TimingEngine } from './TimingEngine.js';
-export { UserArchetypes } from './UserArchetypes.js';
-export { BehaviorSimulator } from './BehaviorSimulator.js';
