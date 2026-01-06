411b022062f8b4aadad7a39b661a0ebea4d40620
OneTrueHomie <vicigwilo@gmail.com>
2026-01-06 21:17:53 +0100

    Remove wallet-farm/src/index.js - moved to core/src/

--- wallet-farm/src/index.js
+++
@@ -1,6 +0,0 @@
-// Wallet Farm - Main entry point
-export { WalletFarm, FundDistributor } from './core/index.js';
-export { BehaviorSimulator, TimingEngine, UserArchetypes } from './simulation/index.js';
-
-// Re-export commonly used utilities
-export { ethers } from 'ethers';
